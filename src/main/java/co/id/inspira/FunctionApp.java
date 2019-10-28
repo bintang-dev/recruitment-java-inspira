acdb6ab4aca5a1b32fe1b70c968289de91889d0c
Aditya Bintang Pratama <limaxs_aditya@yahoo.co.id>
2019-10-28 23:28:07 +0700

    First Init

---
+++ src/main/java/co/id/inspira/FunctionApp.java
@@ -0,0 +1,22 @@
+package co.id.inspira;
+
+public class FunctionApp {
+    //Looping Biasa
+    public static void printFibbonarci(int i){
+
+    }
+
+    public static void printFactorial(int i){
+
+    }
+
+    //Rekursif
+
+    public static int printFibbonarciRecursive(int i){
+        return 0;
+    }
+
+    public static int printFactorialRecursive(int i){
+        return 0;
+    }
+}
---
+++ src/main/java/co/id/inspira/MainApp.java
@@ -0,0 +1,11 @@
+package co.id.inspira;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        FunctionApp.printFactorial(8);
+        FunctionApp.printFactorialRecursive(8);
+        FunctionApp.printFibbonarci(8);
+        FunctionApp.printFibbonarciRecursive(8);
+    }
+}
